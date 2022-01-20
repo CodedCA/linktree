2a995a809114c6b59bd5166b37a5b29498e4dc44
Claudio A <64589027+CodedCA@users.noreply.github.com>
2022-01-20 09:21:17 -0300

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,13 @@
+var r = document.querySelector(':root');
+
+function pink() {
+    r.style.setProperty('--main-color', 'rgb(190, 128, 149)');
+}
+
+function green() {
+    r.style.setProperty('--main-color', 'rgb(128, 190, 141)');
+}
+
+function blue(){
+    r.style.setProperty('--main-color', 'rgb(128, 145, 190)');
+}
